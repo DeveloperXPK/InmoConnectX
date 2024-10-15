63ebe33df34f0fe74474ed9cdef0c09e0cebf521
Andres Giraldo <andresgiraldo05a@gmail.com>
2024-10-15 12:26:54 -0500

    Generacion y validacion de Token, validacion de permiso mediante Rol

---
+++ Backend/helpers/autenticacion.js
@@ -0,0 +1,79 @@
+// Autenticación de usuario
+
+const moment = require('moment');
+const jwt = require('jwt-simple');
+
+// Clave secreta para el token
+const SECRET = '19d1d5941e0f4b4e8b4ee60bfc5c707c94de471db537f9425ee1bd88c7c1f5e6';
+
+// Generacion de token
+function generarToken(usuario) {
+
+    // Cargamos la información del usuario en el token
+    const payload = {
+        sub: usuario._id,
+        name: usuario.nombre,
+        email: usuario.email,
+        rol: usuario.rol,
+
+        // Momento en el que se crea el token
+        iat: moment.unix(),
+
+        // Momento en el que expira el token
+        exp: moment().add(5, 'minutes').unix()
+    };
+
+    // Devolvemos el token generado
+    return jwt.encode(payload, SECRET);
+};
+
+
+// Validacion de token
+function validarToken(req, res, next) {
+
+    // Verificamos que el token se haya enviado
+    try {
+
+        // Extraemos el token de la cabecera de la petición
+        const token = req.headers.authorization.split('')[1];
+
+        // Decodificamos el token
+        const payload = jwt.decode(token, SECRET);
+
+        // Guardamos el id y el rol en el header
+        req.header.usuarioId = payload.sub;
+        req.header.usuarioRol = payload.rol;
+
+        // Contimuamos con la ejecución
+        next();
+
+    } catch {
+        res.status(403).send({ message: 'Error al iniciar sesion' })
+    };
+};
+
+// Validar permiso mediante rol
+function validarPermiso(rolesPermitidos) {
+
+    // Retornamos una funcion que se ejecuta en la ruta
+    return (req, res, next) => {
+
+        // Verificamos los roles permitidos en la cabecera
+        if (rolesPermitidos.includes(req.header.usuarioRol)) {
+
+            // Continuamos con la peticion
+            next();
+        } else {
+
+            // Retornamos un mensaje de que no tiene permiso
+            res.status(403).send({ message: 'No tienes permiso para realizar esta acción' })
+        };
+    };
+};
+
+// Exportamos las funciones
+module.exports = {
+    generarToken,
+    validarToken,
+    validarPermiso
+}
