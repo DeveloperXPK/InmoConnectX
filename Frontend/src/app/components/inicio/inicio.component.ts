beca62b34b188edd9cd29986e2640945200b6295
Andres Giraldo <andresgiraldo05a@gmail.com>
2024-11-04 22:04:59 -0500

    add metodo cargar post, fix template para publicaciones

--- Frontend/src/app/components/inicio/inicio.component.ts
+++ Frontend/src/app/components/inicio/inicio.component.ts
@@ -1,67 +1,66 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AutenticacionService } from '../../services/autenticacion.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AutenticacionService } from "../../services/autenticacion.service";
+import { Posts } from "../../interfaces/posts";
+import { PublicacionesService } from "../../services/publicaciones.service";
 
 @Component({
-  selector: 'app-inicio',
+  selector: "app-inicio",
   standalone: true,
   imports: [],
   template: `
-    @for (inmueble of inmuebles; track inmueble.id) {
-            <div class="property-card">
-                <div class="property-info">
-                    <h2>{{inmueble.name}}</h2>
-                    <h3>{{inmueble.price}}</h3>
-                    <p>{{inmueble.description}}</p>
-                    <a>Saber más</a> 
-                    <!-- Aqui estoy intentando pasar el id para pasar a una pagina donde se muestren los datos de la publicacion -->
-                    <!-- <app-publish-info [inmueble]="inmueble" /> -->
-                </div>
-            </div>
-        }
-
-      <button (click)='logout()'>Cerrar Sesión</button>
+    @if (inmuebles.length > 0) { 
+      @for (inmueble of inmuebles; track
+      inmueble._id) {
+      <div class="property-card">
+        <div class="property-info">
+          <h2>{{ inmueble.titulo }}</h2>
+          <h3>{{ inmueble.precio }}</h3>
+          <p>{{ inmueble.descripcion }}</p>
+          <p>{{ inmueble.ubicacion }}</p>
+          <a>Saber más</a>
+        </div>
+      </div>
+      } 
+    } @else {
+      <h1>No hay inmuebles disponibles</h1>
+    }
+    <button (click)="logout()">Cerrar Sesión</button>
   `,
-  styleUrl: './inicio.component.css',
+  styleUrl: "./inicio.component.css",
 })
-export class InicioComponent {
-  constructor(private router: Router, private autenticacionService: AutenticacionService) {}
+export class InicioComponent implements OnInit {
+  inmuebles: Posts[] = [];
 
-
+  constructor(
+    private router: Router,
+    private autenticacionService: AutenticacionService,
+    private publicacionesService: PublicacionesService
+  ) {}
 
   ngOnInit() {
-    let token = sessionStorage.getItem('session_token');
-    if (!token) {
-      this.router.navigate(['/login']);
+    if (!this.autenticacionService.isAuthenticated()) {
+      this.router.navigate(["/login"]);
+      return;
     }
+
+    this.cargarPosts();
   }
 
-  logout(){
+  cargarPosts() {
+    this.publicacionesService.getPosts().subscribe({
+      next: (data) => {
+        this.inmuebles = data.publicaciones; // data.publicaciones es el nombre del objeto que se recibe del backend
+        console.log("Posts cargados", this.inmuebles);
+      },
+      error: (error) => {
+        console.log("Error al cargar los posts", error);
+      },
+    });
+  }
+
+  logout() {
     this.autenticacionService.clearToken();
-    this.router.navigate(['/login'])
+    this.router.navigate(["/login"]);
   }
-
-  inmuebles = [
-    {
-      id: 1,
-      name: 'Casa en Medellin',
-      price: 500000000,
-      location: 'Medellin',
-      description: 'Casa en Medellin, 3 habitaciones, 2 baños, 1 parqueadero'
-    },
-    {
-      id: 2,
-      name: 'Apartamento en Bogota',
-      price: 300000000,
-      location: 'Bogota',
-      description: 'Apartamento en Bogota, 2 habitaciones, 1 baño, 1 parqueadero'
-    },
-    {
-      id: 3,
-      name: 'Casa en Cali',
-      price: 400000000,
-      location: 'Cali',
-      description: 'Casa en Cali, 4 habitaciones, 3 baños, 2 parqueadero'
-    }
-  ]
 }
