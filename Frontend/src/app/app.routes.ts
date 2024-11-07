b51b7137a728d6d3433ffc197768f48ea49524d3
Andres Giraldo <andresgiraldo05a@gmail.com>
2024-11-07 18:41:43 -0500

    Add ruta de publicacion

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,10 +3,22 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { PublicacionComponent } from './components/publicacion/publicacion.component';
+import { CrearPostComponent } from './components/crear-post/crear-post.component';
 
 export const routes: Routes = [
+
+    // Ruta de inicio
     {path: 'inicio', component: InicioComponent},
+
+    // Ruta de Login
     {path: 'login', component: LoginComponent},
+
+    // Ruta de Registro
     {path: 'register', component: RegisterComponent},
-    {path: 'publicacion', component: PublicacionComponent},
+
+    // Ruta de Publicacion por id
+    {path: 'publicacion/:id', component: PublicacionComponent}, 
+
+    // Rtura de Crear publicacion
+    {path: 'crearPost', component: CrearPostComponent},
 ];
