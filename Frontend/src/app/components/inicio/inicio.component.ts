35b630f79367bb359c44a847096d105984d1fe34
Andres Giraldo <andresgiraldo05a@gmail.com>
2024-11-07 22:35:38 -0500

    Documentacion de metodos

--- Frontend/src/app/components/inicio/inicio.component.ts
+++ Frontend/src/app/components/inicio/inicio.component.ts
@@ -31,42 +31,55 @@ import { CommonModule } from "@angular/common";
   styleUrl: "./inicio.component.css",
 })
 export class InicioComponent implements OnInit {
-  inmuebles: Posts[] = [];
+  inmuebles: Posts[] = []; // Se crea un arreglo de tipo Posts para almacenar los inmuebles
 
+  // Se inyectan los servicios de AutenticacionService y PublicacionesService
   constructor(
     private router: Router,
     private autenticacionService: AutenticacionService,
     private publicacionesService: PublicacionesService
   ) {}
 
+
+  // Metodo que se ejecuta al cargar el componente
   ngOnInit() {
+
+    // Si no hay una sesion activa, se redirige al login
     if (!this.autenticacionService.isAuthenticated()) {
       this.router.navigate(["/login"]);
-      return;
+      return; // Se finaliza la ejecucion del metodo
     }
-
+    
+    // Si existe una sesion activa, se cargan los posts
     this.cargarPosts();
   }
 
+  // Metodo para cargar los posts
   cargarPosts() {
+
+    // Se llama al metodo getPosts del servicio PublicacionesService y se suscribe a la respuesta
     this.publicacionesService.getPosts().subscribe({
       next: (data) => {
         this.inmuebles = data.publicaciones; // data.publicaciones es el nombre del objeto que se recibe del backend
-        console.log("Posts cargados", this.inmuebles);
+        console.log("Posts cargados", this.inmuebles); // Se imprime en consola los posts cargados
       },
+      // En caso de error, se imprime en consola el error
       error: (error) => {
         console.log("Error al cargar los posts", error);
       },
     });
   }
 
+  // Metodo para cerrar sesion
   logout() {
-    this.autenticacionService.clearToken();
-    this.router.navigate(["/login"]);
+    // Se llama al metodo clearSesion del servicio AutenticacionService que limpia los datos de la sesion
+    this.autenticacionService.clearSesion(); 
+    this.router.navigate(["/login"]); // Se redirige al login
   }
 
-
+  // Metodo para ver los detalles de un inmueble 
+  // Recibe como parametro el id del inmueble y asi anexar el id al path de la ruta
   verDetalles(id: string) {
-    this.router.navigate(["/publicacion", id]);
+    this.router.navigate(["/publicacion", id]); // Se redirige a la ruta /publicacion/:id
   }
 }
